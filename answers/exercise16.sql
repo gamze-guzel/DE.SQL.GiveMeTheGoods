e98ae4c0615f2a45d59c75a2e46d605820a68116
Gamze <gamze@zipcodersmbp81.myfiosgateway.com>
2022-07-24 18:42:52 -0400

    final update

---
+++ answers/exercise16.sql
@@ -0,0 +1,4 @@
+Select PostalCode
+from Students AS Zip;
+
+When displaying the Students table, make an alias of the PostalCode column, the column should be called Zip instead.
---
+++ answers/exercise17.sql
@@ -0,0 +1,5 @@
+Select *
+from Students AS Learners;
+
+When displaying the Students table, refer to the table as Learners instead of Students.
+
